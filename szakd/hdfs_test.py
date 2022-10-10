7780eab864faa9679d33cbce990565645be3b8fb
MGregosits <100145203+MGregosits@users.noreply.github.com>
2022-10-10 16:35:26 +0200

    Delete hdfs_test.py

--- szakd/hdfs_test.py
+++
@@ -1,5 +0,0 @@
-import pyhdfs
-
-fs = pyhdfs.HdfsClient(hosts='localhost:9870', user_name='dr.who')
-
-fs.copy_from_local('/home/gmate/szakd/mapmatched_data/2019_03_10__23_57_58__2019_03_17__23_57_58.csv', '/user/web/output/')
